48cccf85163da9730963c53403949bcacab49fbb
SymonBezerra <simaobezerratranslate@gmail.com>
2022-08-13 21:42:29 -0300

    first commit

---
+++ personal_finances.py
@@ -0,0 +1,77 @@
+import json
+
+class Register:
+    # this class can be either an increase\decrease value instance
+    def __init__(self, name, value=float):
+        self.__name = name
+        self.__value = value
+
+    @property
+    def name(self): return self.__name
+
+    @property
+    def value(self): return self.__value
+
+class Month:
+    # storing entries for later .json and\or Pandas.DataFrame
+    def __init__(self, name):
+        self.__inputs = []
+        self.__outputs = []
+        self.__log = {}
+
+        self.__name = name
+
+    @property
+    def log(self): return self.__log
+
+    def add_input (self, name, value=float):
+        self.__inputs.append(Register(name, value))
+
+    def add_output (self, name, value):
+        self.__outputs.append(Register(name, value))
+
+    def add_output_perc(self, name, perc):
+        value = self.balance * (perc / 100)
+        self.__outputs.append(Register(name, value))
+    
+    @property
+    def total_input(self):
+        total_input = 0
+        for input in self.__inputs:
+            total_input += input.value
+        return total_input
+
+    @property
+    def balance(self):
+        total_output = 0
+        for output in self.__outputs:
+            total_output += output.value
+        return self.total_input - total_output
+
+    def create_log(self):
+        all_values = []
+        all_ops = []
+        all_names = []
+        for input in self.__inputs:
+            all_values.append(input.value)
+            all_ops.append("input")
+            all_names.append(input.name)
+        for output in self.__outputs:
+            all_values.append(output.value)
+            all_ops.append("output")
+            all_names.append(output.name)
+        
+        all_names.append("Balance")
+        all_ops.append("---")
+        all_values.append(self.balance)
+
+        self.__log["Name"] = all_names
+        self.__log["Op"] = all_ops
+        self.__log["Value"] = all_values
+
+        return self.log
+
+    def log_file (self):
+        outfile = open(f"{self.__name}.json", "w")
+        outfile.write(json.dumps(self.log, indent=4))
+        outfile.close()
