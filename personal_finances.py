1a4ebb18eda919ee4f8cc91eeb363018018ecb12
SymonBezerra <simaobezerratranslate@gmail.com>
2022-09-07 11:05:55 -0300

    added type annotations

--- personal_finances.py
+++ personal_finances.py
@@ -2,7 +2,7 @@ import json
 
 class Register:
     # this class can be either an increase\decrease value instance
-    def __init__(self, name, value=float):
+    def __init__(self, name: str, value: float) -> None:
         self.__name = name
         self.__value = value
 
@@ -14,7 +14,7 @@ class Register:
 
 class Month:
     # storing entries for later .json and\or Pandas.DataFrame
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         self.__inputs = []
         self.__outputs = []
         self.__log = {}
@@ -22,33 +22,33 @@ class Month:
         self.__name = name
 
     @property
-    def log(self): return self.__log
+    def log(self) -> dict: return self.__log
 
-    def add_input (self, name, value=float):
+    def add_input (self, name: str, value: float):
         self.__inputs.append(Register(name, value))
 
-    def add_output (self, name, value):
+    def add_output (self, name:str, value: float):
         self.__outputs.append(Register(name, value))
 
-    def add_output_perc(self, name, perc):
+    def add_output_perc(self, name: str, perc: float):
         value = self.balance * (perc / 100)
         self.__outputs.append(Register(name, value))
     
     @property
-    def total_input(self):
+    def total_input(self) -> float:
         total_input = 0
         for input in self.__inputs:
             total_input += input.value
         return total_input
 
     @property
-    def balance(self):
+    def balance(self) -> float:
         total_output = 0
         for output in self.__outputs:
             total_output += output.value
         return self.total_input - total_output
 
-    def create_log(self):
+    def create_log(self) -> list:
         all_values = []
         all_ops = []
         all_names = []
@@ -71,7 +71,7 @@ class Month:
 
         return self.log
 
-    def log_file (self):
+    def log_file (self) -> None:
         outfile = open(f"{self.__name}.json", "w")
         outfile.write(json.dumps(self.log, indent=4))
         outfile.close()
